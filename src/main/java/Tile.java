a8a85baa4ebffbf43430181e8eb23497d6d94a8a
palaszwaldemar <palasz.waldemar0@gmail.com>
2022-12-22 13:42:31 +0100

    refactor image loading

--- src/main/java/Tile.java
+++ src/main/java/Tile.java
@@ -6,10 +6,16 @@ import java.io.IOException;
 public class Tile {
     private final int x;
     private final int y;
+    private BufferedImage bufferedImage;
 
     public Tile(int x, int y) {
         this.x = x;
         this.y = y;
+        try {
+            bufferedImage = ImageIO.read(Tile.class.getResource("/tiles/PNG/Base_Game_C2_Tile_D.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public int getX() {
@@ -21,14 +27,6 @@ public class Tile {
     }
 
     void render(Graphics g) {
-        g.setColor(Color.GREEN);// CHECK : 09.12.2022 dodano na eksperymenty
-        g.fillRect(Cords.xToPixels(x), Cords.yToPixels(y), 100, 100);// CHECK : 09.12.2022 dodano na eksperymetny
-        /*g.drawRect(Cords.xToPixels(x), Cords.yToPixels(y), 100, 100);*/ // CHECK : 09.12.2022 zakomentowano na eksperymenty
-        try {
-          BufferedImage bufferedImage = ImageIO.read(Tile.class.getResource("/tiles/PNG/Base_Game_C2_Tile_D.png"));
-          g.drawImage(bufferedImage, Cords.xToPixels(x), Cords.yToPixels(y), 100, 100, null);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        g.drawImage(bufferedImage, Cords.xToPixels(x), Cords.yToPixels(y), 100, 100, null);
     }
 }

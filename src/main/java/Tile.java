d321345dbd77448ee67161f55cb276ede78331fd
palaszwaldemar <palasz.waldemar0@gmail.com>
2022-12-22 13:34:44 +0100

    simple load image

--- src/main/java/Tile.java
+++ src/main/java/Tile.java
@@ -1,4 +1,7 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Tile {
     private final int x;
@@ -21,5 +24,11 @@ public class Tile {
         g.setColor(Color.GREEN);// CHECK : 09.12.2022 dodano na eksperymenty
         g.fillRect(Cords.xToPixels(x), Cords.yToPixels(y), 100, 100);// CHECK : 09.12.2022 dodano na eksperymetny
         /*g.drawRect(Cords.xToPixels(x), Cords.yToPixels(y), 100, 100);*/ // CHECK : 09.12.2022 zakomentowano na eksperymenty
+        try {
+          BufferedImage bufferedImage = ImageIO.read(Tile.class.getResource("/tiles/PNG/Base_Game_C2_Tile_D.png"));
+          g.drawImage(bufferedImage, Cords.xToPixels(x), Cords.yToPixels(y), 100, 100, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
